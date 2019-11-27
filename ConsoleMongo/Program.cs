828c16e5149c07611a05a93e9d12713120561257
Kleiton C. Tangioni <kleitonct@hotmail.com>
2019-11-26 21:47:05 -0300

    commit teste

---
+++ ConsoleMongo/Program.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace ConsoleMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cliente = new MongoClient("mongodb://localhost:27017");
+
+            var db = cliente.GetDatabase("net16");
+
+            var col = db.GetCollection<BsonDocument>("col01");
+
+            col.Find(new BsonDocument() {
+                { "nome", 1 }
+            });
+
+
+            var insert = new BsonDocument()
+            {
+                { "nome", "Kleiton Cunha" },
+                { "array", new BsonArray()
+                            {
+                                "MS", "FIAP"
+                            }
+                }
+            };
+        }
+    }
+}
--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,19 @@ namespace SimpleBotCore.Logic
     {
         public string Reply(SimpleMessage message)
         {
+            var cliente = new MongoClient("mongodb://localhost:27017");
+
+            var db = cliente.GetDatabase("net16");
+
+            var col = db.GetCollection<MongoDB.Bson.BsonDocument>("col02");
+
+            col.InsertOne(new MongoDB.Bson.BsonDocument()
+            {
+                { "id", message.Id },
+                { "user", message.User },
+                { "message", message.Text }
+            });
+
             return $"{message.User} disse '{message.Text}'";
         }
 
